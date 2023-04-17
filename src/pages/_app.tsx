521ec3853bdddc1e5595d4b6c5cb0a3502fb856e
merklefruit <nicolas@racchi.com>
2023-04-17 08:20:56 +0200

    feat: added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 import type { AppProps } from "next/app";
 
@@ -12,6 +13,16 @@ export default function App({ Component, pageProps }: AppProps) {
       className={`bg-gradient-to-t from-moonlightInterface to-moonlightBase 
       min-h-screen text-moonlightWhite ${inter.className}`}
     >
+      <Head>
+        <script
+          defer
+          id="pirschjs"
+          type="text/javascript"
+          src="https://api.pirsch.io/pirsch.js"
+          data-code="SOAHHIcBiJFDKELHN3dz6Y1XUty0anTs"
+        />
+      </Head>
+
       <Component {...pageProps} />
     </div>
   );
