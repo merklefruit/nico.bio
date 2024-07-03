877f85c5814c8e432638f448491d26529f6f66d9
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-07-03 20:11:06 +0200

    chore: email change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,19 @@ export default function Home() {
     <BaseLayout title="About" icon={<Fingerprint />}>
       <div>
         <p className="text-monochromeCloud font-light">
-          Hi! I&apos;m Nicolas, an engineer and blockchain enthusiast.
-          <br /> Reach out on X if you want to chat 🌱
+          I&apos;m Nicolas, a blockchain engineer and researcher focusing on the
+          Ethereum ecosystem.
+          <br />
+          <br />
+          I&apos;m working on latency-optimized infrastructure for MEV at{" "}
+          <a
+            className="hover:opacity-80 text-moonlightBlue decoration-dotted underline decoration-moonlightBlue underline-offset-1"
+            href="https://www.chainbound.io/"
+            target="_blank"
+          >
+            Chainbound
+          </a>
+          , and I love contributing to open-source projects to learn stuff.
         </p>
       </div>
 
@@ -40,6 +51,22 @@ export default function Home() {
             target="_blank"
           >
             @merklefruit
+          </a>
+        </div>
+
+        <div className="flex gap-3.5">
+          <div className="flex gap-2">
+            <Mail strokeWidth={1.5} size={19} className="mt-0.5" />
+            <p>Email</p>
+          </div>
+          <div className="w-full border-b border-moonlightSoft mb-2.5" />
+          <a
+            className="hover:text-moonlightStone underline decoration-moonlightStone 
+            underline-offset-1 min-w-max"
+            href="mailto:merklefruit@proton.me"
+            target="_blank"
+          >
+            merklefruit@proton.me
           </a>
         </div>
       </div>
